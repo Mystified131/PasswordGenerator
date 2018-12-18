ae6e35f2129fd152718ae4eca6b5708f3c79543c
mystified131 <mystifiedthomas@gmail.com>
2018-12-18 05:13:07 -0600

    Initial Commit.

---
+++ MVCApplication/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCApplication.Models;
+using MVCApplication.ViewModels;
+
+namespace MVCApplication.Controllers
+{
+  
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult Result()
+        {
+            ResultViewModel resultViewModel = new ResultViewModel();
+
+            return View(resultViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Result(ResultViewModel resultViewModel)
+
+        {
+            if (ModelState.IsValid)
+            {
+
+                RandomValue qbert = new RandomValue("qbert");
+
+                resultViewModel.RandomPassword = qbert.RandomPassword();
+
+                return View(resultViewModel);
+            }
+
+            return Redirect("/");
+        }
+
+    }
+
+
+
+}
